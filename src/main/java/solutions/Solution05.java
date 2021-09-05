a1734b6981aa9c2ac4362578fbe292d398d4e2bd
Zaher <ilbihz@gmail.com>
2021-09-04 23:24:41 -0400

    unchallenging the solution

--- src/main/java/solutions/Solution05.java
+++ src/main/java/solutions/Solution05.java
@@ -14,8 +14,6 @@ public class Solution05 {
         create two int variables 'number1' and 'number2'
         prompt User for 'num1' and 'num2'
         take in 'num1' and 'num2'
-            Challenge: dont allow numbers to be negative
-                loop that redoes what is above if numbers are negative after parsing
         parse 'num1' and 'num2' to integers
     Print the sum, difference, product, and quotient of those numbers as shown in the example output:
         print 'num1'
@@ -33,20 +31,8 @@ public class Solution05 {
         number1 = Integer.parseInt(num1);
         number2 = Integer.parseInt(num2);
 
-        while(number1 <0 || number2 < 0 ){ //loop that ensures the values are non-negative
 
-            System.out.println("The numbers need to be non-negative!");
-            System.out.print("What is the first number? "); //prompt for 'num1
-            num1 =scan.nextLine();
-            System.out.print("What is the second number? "); //prompt for 'num2'
-            num2 =scan.nextLine();
-
-            number1=Integer.parseInt(num1);
-            number2=Integer.parseInt(num2);
-
-        }
-
-        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %d",number1,number2,number1+number2,number1,number2,number1-number2,number1,number2,number1*number2,number1,number2,number1/number2);
+        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %.2f",number1,number2,number1+number2,number1,number2,number1-number2,number1,number2,number1*number2,number1,number2,number1*1.0/number2);
 
     }
 }
